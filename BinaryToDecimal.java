043e9b4032c8413a488c855bd0d109e1727452f5
Büşra Gökmen <37721917+newsteps8@users.noreply.github.com>
2019-09-23 03:28:24 +0300

    Add files via upload

---
+++ BinaryToDecimal.java
@@ -0,0 +1,165 @@
+import java.math.BigInteger;
+
+public class BinaryToDecimal {
+    public static void main(String args[]) {
+
+
+        BigInteger üstelsayı = new BigInteger("183");
+        BigInteger modsayısı = new BigInteger("187");
+        BigInteger result1 = üstelsayı.pow(107).mod(modsayısı);
+
+        System.out.println(result1);
+        }
+
+
+}
+/*      }
+
+
+
+                      else if(arrnew[k].charAt(s)=='0') {
+                       term+=0;
+
+                   }
+                 else if(arrnew[k].charAt(s)=='1')
+                       if s =0;
+                       for1(int o=(k+1)*8-4; o<=(k+1)*8-4-4; 8*(k+1)-4--)
+                       array = 0001;
+                       term += array.charAt(i to 4)*Math.pow(2,o);
+                           else if s = 1
+                       for()
+                           1* 2^k+1*8                                           e
+
+                   else if(arrnew[k].charAt(s)=='e' ) {
+                            String array[] = new String[1];
+                            array[0] = "1110";
+                            if (s == 0) {
+                                for (o = nter; o <= nter-2; nter--) {//
+                                    term +=  1 * Math.pow(2, o);}
+
+                                }}
+
+                             else if (s == 1) {
+                               for (o = iter; o <= iter-2; iter--) {//
+                                    term +=  1 * Math.pow(2, o);}
+                            }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                else if(arrnew[k].charAt(s)=='4' ) {
+                    if s =0;
+                    for1(int o=(k+1)*8-4; o<=(k+1)*8-4-4; 8*(k+1)-4--)
+                    array = 0100;
+                    term += array.charAt(i to 4)*Math.pow(2,o);
+                           else if s = 1
+                    for()
+                        1* 2^k+1*8
+
+                }
+                else if(arrnew[k].charAt(s)=='9' ) {
+                    String array[] = new String[1];
+                            array[0] = "1001";
+                            if (s == 0 && k == 0) {
+                                term += -1 * Math.pow(2, nter);
+
+                                for (int o = nter - 1; o <= nter - 2; nter--) {//
+                                    term += 0 * Math.pow(2, o);
+                                }
+                                term += 1 * Math.pow(2, nter-3);}
+                                if (s == 0 && k != 0) {
+
+                                        term += 1 * Math.pow(2, nter) + 1 * Math.pow(2, nter-3);
+
+
+                                } else if (s == 1) {
+                                     term += 1 * Math.pow(2, nter) + 1 * Math.pow(2, nter-3);
+                                }
+
+                            }
+
+                }
+                else if(arrnew[k].charAt(s)=='3' ) {
+                    if s =0;
+                    for1(int o=(k+1)*8-4; o<=(k+1)*8-4-4; 8*(k+1)-4--)
+                    array = 0011;
+                    term += array.charAt(i to 4)*Math.pow(2,o);
+                           else if s = 1
+                    for()
+                        1* 2^k+1*8
+
+                }
+                else if(arrnew[k].charAt(s)=='7' ) {
+                    if s =0;
+                    for1(int o=(k+1)*8-4; o<=(k+1)*8-4-4; 8*(k+1)-4--)
+                    array = 0111;
+                    term += array.charAt(i to 4)*Math.pow(2,o);
+                           else if s = 1
+                    for()
+                        1* 2^k+1*8
+
+                }
+                else if(arrnew[k].charAt(s)=='8' ) {
+                    if s =0;
+                    for1(int o=(k+1)*8-4; o<=(k+1)*8-4-4; 8*(k+1)-4--)
+                    array = 1000;
+                    term += array.charAt(i to 4)*Math.pow(2,o);
+                           else if s = 1
+                    for()
+                        1* 2^k+1*8
+
+                }
+                else if(arrnew[k].charAt(s)=='e' || arrnew[k].charAt(s)=='E') {
+                    if s =0;
+                    for1(int o=(k+1)*8-4; o<=(k+1)*8-4-4; 8*(k+1)-4--)
+                    array = 0000;
+                    term += array.charAt(i to 4)*Math.pow(2,o);
+                           else if s = 1
+                    for()
+                        1* 2^k+1*8
+
+                }
+                }
+
+            }
+
+
+            }
+            else if(datatype.equals("Unsigned integer")){
+
+            }
+            else if (datatype.equals("Floating point")){
+
+            }
+
+
+        }
+
+
+
+        else if (ordering.equals("Big Endian")){
+
+        }*/
+// int num = (Integer.parseInt(arrnew.charAt(1), 16));
+//System.out.print(Integer.toBinaryString(num));
